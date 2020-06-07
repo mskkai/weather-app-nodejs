702f17e5ad0cfc01092b1c5755333e66def2c8ea
mskkai <msasikumarbeeee@gmail.com>
2020-06-07 22:42:23 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('This is loaded from app.js');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// });
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#msg-1');
+const messageTwo = document.querySelector('#msg-2');
+
+messageOne.textContent = ""
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    });
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=d363e552b8e7d3e508efde82b7f5f6eb&query=' + latitude + ',' + longitude;
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!!');
+        } else if (body.error) {
+            callback('Unable to find location');
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out.' + ' It feels like ' + body.current.feelslike + ' degress out.');
+        }
+    });
+}
+
+module.exports = forecast;
